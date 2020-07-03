be33364e681665f58cc6b79ba0ae6f59a7872295
tamaramunoz <tami.skt@gmail.com>
2020-07-03 00:59:04 -0400

    env variables config

--- backend/database.js
+++ backend/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/javascriptdb', {
+mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,7 @@
+if(process.env.NODE_ENV !== 'production'){
+    require('dotenv').config();
+}
+
 const express = require('express');
 const morgan = require('morgan');
 const multer = require('multer');
@@ -8,7 +12,7 @@ const app = express();
 require('./database');
 
 // settings
-app.set('port', 3000);
+app.set('port', process.env.PORT || 3000);
 
 // middlewares
 app.use(morgan('dev'));
