95faba8a09331c5eb54bd316ca505f8c445857f2
tamaramunoz <tami.skt@gmail.com>
2020-07-03 01:16:37 -0400

    starting config API with POST

--- backend/routes/books.js
+++ backend/routes/books.js
@@ -1,6 +1,26 @@
 const { Router } = require('express');
 const router = Router();
 
-router.get('/', (req, res) => res.json({text: 'hello world'}));
+const Book = require('../models/Books');
+
+router.get('/', async(req, res) => {
+    const books = await Book.find();
+    res.json(books)
+});
+
+router.post('/', async(req, res) => {
+    console.log(req.body);
+    res.send('received')
+});
+
+router.put('/', async(req, res) => {
+    console.log(req.body);
+    res.send('updated')
+});
+
+router.delete('/', async(req, res) => {
+    console.log(req.body);
+    res.send('deleted')
+});
 
 module.exports = router;
