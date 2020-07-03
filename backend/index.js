3da6b28ddc61fc9214e020f5b4dad67d327407db
tamaramunoz <tami.skt@gmail.com>
2020-07-03 00:12:25 -0400

    middlewares config

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,25 @@
 const express = require('express');
 const morgan = require('morgan');
+const multer = require('multer');
+const path = require('path');
 
 // initializations
 const app = express();
 
-// middlewares
-
-
 // settings
 app.set('port', 3000);
+
+// middlewares
+app.use(morgan('dev'));
+const storage = multer.diskStorage({
+    destination: path.join(__dirname, 'public/uploads'),
+    filename(req, file, cb){
+        cb(null, new Date().getTime() + path.extname(file.originalname));
+    }
+});
+app.use(multer(storage).single('image'));
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
 
 // start server
 app.listen(app.get('port'), () => {
