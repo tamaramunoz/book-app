69044fd39ab7ad77949cfcfa67d3064762dee2e1
tamaramunoz <tami.skt@gmail.com>
2020-07-06 14:21:33 -0400

    saving images and related info

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,7 @@ const express = require('express');
 const morgan = require('morgan');
 const multer = require('multer');
 const path = require('path');
+const cors = require('cors')
 
 // initializations
 const app = express();
@@ -21,10 +22,11 @@ const storage = multer.diskStorage({
     filename(req, file, cb){
         cb(null, new Date().getTime() + path.extname(file.originalname));
     }
-});
-app.use(multer(storage).single('image'));
-app.use(express.urlencoded({extended: false}));
+})
+app.use(multer({ storage }).single('image'));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(cors());
 
 // routes
 app.use('/api/books', require('./routes/books'));
--- backend/routes/books.js
+++ backend/routes/books.js
@@ -10,7 +10,8 @@ router.get('/', async(req, res) => {
 
 router.post('/', async(req, res) => {
     const { title, author, isbn } = req.body;
-    const newBook = new Book({ title, author, isbn });
+    const imagePath = '/uploads/' + req.file.filename;
+    const newBook = new Book({ title, author, isbn, imagePath});
     await newBook.save();
     res.json({message: 'Book Saved'});
 });
