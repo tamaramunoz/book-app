b1bf69ba4876c33972c5537f1eacb578845ad8e0
tamaramunoz <tami.skt@gmail.com>
2020-07-06 13:52:55 -0400

    function bookservice

--- frontend/app.js
+++ frontend/app.js
@@ -1,4 +1,5 @@
 import './styles/app.css';
+import BookService from './services/BookService';
 
 document.getElementById('book-form')
     .addEventListener('submit', e => {
@@ -6,6 +7,15 @@ document.getElementById('book-form')
         const author = document.getElementById('author').value;
         const isbn = document.getElementById('isbn').value;
         const image = document.getElementById('image').files;
+
+        const formData = new FormData();
+        formData.append('image', image[0]);
+        formData.append('title', title);
+        formData.append('author', author);
+        formData.append('isbn', isbn);
+
+        const bookService = new BookService()
+        bookService.postBooks(formData)
 
         e.preventDefault();
         console.log(title, author, isbn, image);
---
+++ frontend/services/BookService.js
@@ -0,0 +1,34 @@
+class BookService {
+    
+    constructor(){
+        this.URI = 'http://localhost:3000/api/books';
+    }
+
+    async getBooks() {
+        const response = await fetch(this.URI);
+        const books = await response.json();
+        return books;
+    }
+
+    async postBooks(book) {
+        const response = await fetch(this.URI, {
+            method: 'POST',
+            body: book
+        });
+        const data = await response.json();
+        console.log(data);
+    }
+
+    async deleteBooks(bookId) {
+        const response = await fetch(`${this.URI}/${bookId}`, {
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            method: 'DELETE',
+        });
+        const data = await response.json();
+        console.log(data);
+    }
+}
+
+module.exports = BookService;
