fbbf8a6488b6f59253a75d1062c55324e75469dc
tamaramunoz <tami.skt@gmail.com>
2020-07-06 13:27:57 -0400

    function to get elements by id from html

--- frontend/app.js
+++ frontend/app.js
@@ -1,1 +1,12 @@
 import './styles/app.css';
+
+document.getElementById('book-form')
+    .addEventListener('submit', e => {
+        const title = document.getElementById('title').value;
+        const author = document.getElementById('author').value;
+        const isbn = document.getElementById('isbn').value;
+        const image = document.getElementById('image').files;
+
+        e.preventDefault();
+        console.log(title, author, isbn, image);
+    });
