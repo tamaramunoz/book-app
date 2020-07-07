a1e24d790a1899a6d5b5466bcf5a8c7b514ee018
tamaramunoz <tami.skt@gmail.com>
2020-07-07 13:19:22 -0400

    deleting image from public/upload folder

--- backend/routes/books.js
+++ backend/routes/books.js
@@ -1,5 +1,7 @@
 const { Router } = require('express');
 const router = Router();
+const { unlink } = require('fs-extra');
+const path = require('path');
 
 const Book = require('../models/Books');
 
@@ -17,7 +19,8 @@ router.post('/', async(req, res) => {
 });
 
 router.delete('/:id', async(req, res) => {
-    await Book.findByIdAndDelete(req.params.id);
+    const book = await Book.findByIdAndDelete(req.params.id);
+    unlink(path.resolve('./backend/public' + book.imagePath))
     res.json({message: 'Book Deleted'});
 });
 
